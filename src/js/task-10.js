e671ba77ddd1a5208da0c6024b43cc41da11dcf5
Mateusz <135542253+Mateusz42@users.noreply.github.com>
2023-12-21 23:05:34 +0100

    Update task-10.js

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -1,5 +1,37 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
-    .padStart(6, 0)}`;
+    .padStart(6, '0')}`;
+}
+
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createBtn.addEventListener('click', () => {
+  const amount = document.querySelector('input[type="number"]').valueAsNumber;
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener('click', () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  destroyBoxes();
+
+  let boxSize = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.style.width = `${boxSize + 10 * i}px`;
+    box.style.height = `${boxSize + 10 * i}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.marginBottom = '5px';
+    boxesContainer.appendChild(box);
+    boxHeight += 10;
+  }
+}
+
+function destroyBoxes() {
+  boxesContainer.innerHTML = '';
 }
